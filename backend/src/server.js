7192eb5fb1129cd5e6a88e855e830476a4568d1b
Aswin-S-T <aswinswebart15@gmail.com>
2025-03-07 01:20:57 +0530

    development: basic folder structure created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,20 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/users/userRouter");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// Middlewares
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes configurations
+app.use("/api/v1/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Nodejs server is running....");
