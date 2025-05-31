1f202cf55b3d1dfdd722a74c9c4e5b9eb8c852c4
Aswin-S-T <aswinswebart15@gmail.com>
2025-05-31 17:17:57 +0530

    cors issue fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,7 @@ const limiter = rateLimit({
 
 var corsOptions = {
   // origin: "http://localhost:3000", // For dev
-  origin: "https://wheelzloop.com/",
+  origin: "https://wheelzloop.com",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
