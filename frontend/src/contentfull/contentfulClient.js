73c1def7792f47f17b8f64b48a89150bf9c3f7df
Aswin-S-T <aswinswebart15@gmail.com>
2025-06-13 10:23:44 +0530

    contentfull changes

--- frontend/src/contentfull/contentfulClient.js
+++ frontend/src/contentfull/contentfulClient.js
@@ -1,16 +1,9 @@
 import { createClient } from "contentful";
 const contentful = require("contentful");
 
-// const client = createClient({
-//   space: process.env.REACT_APP_CONTENTFULL_SPACEID, 
-//   environment: "master",
-//   accessToken: process.env.REACT_APP_CONTENTFULL_ACCESS_TOKEN, 
-// });
-
-
 const client = createClient({
-  space: 'your_space_id_here',
-  accessToken: 'your_access_token_here',
+  space: process.env.REACT_APP_CONTENTFULL_SPACEID,
+  accessToken: process.env.REACT_APP_CONTENTFULL_ACCESS_TOKEN,
 });
 
 export default client;
@@ -23,7 +16,3 @@ export const fetchEntries = async () => {
     console.error("Error fetching entries:", error);
   }
 };
-
-// client.getEntry('18Y6zz1f3I4lsDrUnMa2Q6')
-//   .then((entry) => console.log(entry))
-//   .catch(console.error)
