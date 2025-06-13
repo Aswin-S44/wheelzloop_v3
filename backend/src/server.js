f13e411da218d8ab3876dd494aa74f1e049fed13
Aswin-S-T <aswinswebart15@gmail.com>
2025-06-13 20:57:24 +0530

    cors origin changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,8 +26,8 @@ const limiter = rateLimit({
 });
 
 var corsOptions = {
-  origin: "http://localhost:3000", // For dev
-  // origin: "https://wheelzloop.com",
+  // origin: "http://localhost:3000", // For dev
+  origin: "https://wheelzloop.com",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
